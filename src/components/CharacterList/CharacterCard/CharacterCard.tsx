65c2dd9ce27de425c79e6868a06c833b8f8cc544
Josie Ko <josipakukor@gmail.com>
2024-11-13 20:22:14 +0100

    Feat: add CharacterCard component
    Added new component and styles for character list items

---
+++ src/components/CharacterList/CharacterCard/CharacterCard.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./CharacterCard.module.scss";
+
+interface CharacterCardProps {
+    name: string;
+    title: string;
+    description: string;
+    avatarUrl: string;
+}
+
+export const CharacterCard = ({ name, title, description, avatarUrl }: CharacterCardProps) => {
+    return (
+        <div className={styles.characterCard}>
+            <div className={styles.characterAvatarContainer}>
+                <img src={avatarUrl} alt={name} className={styles.characterAvatar} />
+            </div>
+            <div className={styles.characterInfoContainer}>
+                <div className={styles.characterHeader}>
+                    <div className={styles.characterName}>
+                        <h2>{name}</h2>
+                        <p className={styles.characterCategory}>
+                            {title.charAt(0).toUpperCase() + title.slice(1)}
+                        </p>
+                    </div>
+                </div>
+                <p className={styles.characterDescription}>{description}</p>
+            </div>
+        </div>
+    );
+};
